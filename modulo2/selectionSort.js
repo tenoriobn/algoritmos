ce6db66ed0799f40d709a71bc408ab487a099d60
Bruno Tenório <tenoriobn@gmail.com>
2023-08-25 10:11:11 -0300

    Selection Sort

--- modulo2/selectionSort.js
+++ modulo2/selectionSort.js
@@ -1,6 +1,17 @@
-const livros = require("../modulo1/listaLivros");
+const livros = require("./listaLivros");
 const menorValor = require("./menorValor");
 
-for (let atual = 0; atual < livros.length; atual++) {
-    
+for (let atual = 0; atual < livros.length - 1; atual++) {
+    let menor = menorValor(livros, atual);
+
+    let livroAtual = livros[atual];
+    console.log('posição atual', atual)
+    console.log('livro atual', livros[atual])
+    let livroMenorPreco = livros[menor];
+    console.log('livro menor preço', livros[menor])
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
 }
+
+console.log(livros);
