3dbe435fe8b1a1bfe1ba4f8c1fc5b1c88a8ff2dc
Bruno Tenório <tenoriobn@gmail.com>
2023-08-28 13:10:35 -0300

    Fazendo 'teste de mes' direto no projeto

--- modulo3/insertionSort.js
+++ modulo3/insertionSort.js
@@ -1,13 +1,28 @@
+// Importando a array de objetos dos produtos.
 const livros = require('./listaLivros');
+// Importando a função
 const troca = require('./troca');
 
+// Função que irá receber a lista
 function insertionSort(lista) {
     
+    // Aqui é iniciado a estrutura para iniciar do 0 até o último elemento/objeto da lista.
     for(let atual = 0; atual < lista.length; atual++) {
+        // Aqui analise vai armazenar o valor de `atual` que é incrementado de 1 em 1.
+        // será utilizada para acessar o item anterior e o atual, possibilitando a comparação de ambos
         let analise = atual;
 
+        // Aqui é utilizado a estrutura `enquanto` que só será true, se o valor armazenado em `analise` 
+        // for maior que 1 para que ao fazer `analise--` ele não vá para o indice -1 e ocorra erro.
+        // Além disso, só será executada, se o preço do item atual for menor que o preço do item anterior
         while(analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
+            // Aqui é chamada a função externa responsável pro fazer a troca
+            // tornando a lista ordenada
             troca(lista, analise);
+
+            // Aqui a lista é decrementada, então se estou atualmente no 2 e foi feito a troca
+            // voltamos para o 1, é feito a comparação e se for false, volta para a estrutura for
+            // que vai avançar para o 3.
             analise--
         }
     }
@@ -15,4 +30,5 @@ function insertionSort(lista) {
     console.log(lista)
 }
 
+// Passando a lista de objetos `livros` para a função.
 insertionSort(livros)
--- modulo3/troca.js
+++ modulo3/troca.js
@@ -1,9 +1,18 @@
+// Essa função vai receber a lista dos produtos
+// além disso, vai receber a variavel `analise` que irá armazenar os indices
+// conforme o for foi incrementando de 1 em 1.
 function troca(lista, analise) {
+    // Aqui está sendo armazenado na variável o item correspondente ao indice armazenado em `analise`;
     let itemAnalise = lista[analise];
+    // Aqui está sendo armazenado o item anterior ao indice armazenado em analise, ou seja,
+    // se analise armazena 2, então aqui com -1 está sendo pego o item do indice 1.
     let itemAnterior = lista[analise - 1]; 
 
+    // Aqui é feito a troca, agora analise armazena o item anterior `- 1` que seguindo o exemplo a cima é 1.
     lista[analise] = itemAnterior;
+    // Aqui a troca acontecem, fazendo com que o item do exemplo a cima 2, passe a ter o indice 1.
     lista[analise - 1] = itemAnalise;
 }
 
+// Aqui a função é exportada para ser utilizada em outros arquivos.
 module.exports = troca;
